4431f46ea5e60c2d4089b2901f143f936dc8c7f3
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-09-05 19:13:01 +0100

    added and tested features for deposit and withdrawal

--- Bank/lib/account.rb
+++ Bank/lib/account.rb
@@ -1,2 +1,16 @@
 class Account
+
+  attr_reader :balance
+
+  def initialize initial_balance = 0
+    @balance = initial_balance
+  end
+
+  def deposit amount
+    @balance += amount
+  end
+
+  def withdraw amount
+    @balance -= amount
+  end
 end
--- Bank/spec/account_spec.rb
+++ Bank/spec/account_spec.rb
@@ -9,5 +9,30 @@ describe Account do
     it 'returns a new account object' do
       expect(@account).to be_an_instance_of(Account)
     end
+
+    it 'has a default balance of zero' do
+      expect(@account.balance).to eql(0)
+    end
+
+    it 'has an initial balance' do
+      account = Account.new 100
+      expect(account.balance).to eql(100)
+    end
+  end
+
+  describe 'capital management' do
+    it 'lets you deposit an amount' do
+      expect { @account.deposit(2) }.to change { @account.balance }.by(2)
+    end
+
+    it 'lets you add a new deposit' do
+      @account.deposit(2)
+      @account.deposit(10)
+      expect(@account.balance).to eql(12)
+    end
+
+    it 'lets you withdraw an amount' do
+      expect { @account.withdraw(2) }.to change { @account.balance }.by(-2)
+    end
   end
 end
